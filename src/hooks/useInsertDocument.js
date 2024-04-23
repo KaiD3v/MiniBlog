971b1fc8ce8f5db100e9d1915b224103a2b2ed6a
Kaique Ricardo de Melo <kaikricardo99@gmail.com>
2024-04-23 15:42:58 -0300

    create post function

---
+++ src/hooks/useInsertDocument.js
@@ -0,0 +1,68 @@
+import { useState, useEffect, useReducer } from "react";
+import { db } from "../firebase/config";
+import { collection, addDoc, Timestamp } from "firebase/firestore";
+
+const initalState = {
+  loading: null,
+  error: null,
+};
+
+const insertReducer = (state, action) => {
+  checkCancelledBeforeDispatch({
+    type: "LOADING",
+  });
+
+  switch (action.type) {
+    case "LOADING":
+      return { loading: true, error: null };
+    case "INSERTED_DOC":
+      return { loading: false, error: null };
+    case "ERROR":
+      return { loading: false, error: action.payload };
+    default:
+      return state;
+  }
+};
+
+export const useInsertDocument = (docCollection) => {
+  const [response, dispatch] = useReducer(insertReducer, initalState);
+
+  // lidar com memory leak
+  const [cancelled, setCancelled] = useState(false);
+
+  const checkCancelledBeforeDispatch = (action) => {
+    if (!cancelled) {
+      dispatch(action);
+    }
+  };
+
+  const insertDocument = async (document) => {
+    try {
+      const newDocument = { ...document, createdAt: Timestamp.now() };
+
+      const insertedDocument = await addDoc(
+        collection(db, docCollection),
+        newDocument
+      );
+
+      checkCancelledBeforeDispatch({
+        type: "INSERTED_DOC",
+        payload: insertedDocument,
+      });
+    } catch (error) {
+      checkCancelledBeforeDispatch({
+        type: "ERROR",
+        payload: error.message,
+      });
+    }
+  };
+
+  useEffect(() => {
+    setCancelled(true);
+  }, []);
+
+  return {
+    insertDocument,
+    response,
+  };
+};
--- src/pages/CreatePost/CreatePost.jsx
+++ src/pages/CreatePost/CreatePost.jsx
@@ -1,11 +1,86 @@
-import styles from './CreatePost.module.css'
+import styles from "./CreatePost.module.css";
+
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { useAuthValue } from "../../context/AuthContext";
 
 const CreatePost = () => {
-  return (
-    <div>
-      <h1>Create Post</h1>
-    </div>
-  )
-}
+  const [title, setTitle] = useState("");
+  const [image, setImage] = useState("");
+  const [body, setBody] = useState("");
+  const [tags, setTags] = useState([]);
+  const [formError, setFormError] = useState("");
 
-export default CreatePost
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <div className={styles.create_post}>
+      <h2>Criar post</h2>
+      <p>Escreva sobre o que você quiser e compartilhe o seu conhecimento!</p>
+      <form onSubmit={handleSubmit}>
+        <label>
+          <span>Título:</span>
+          <input
+            type="text"
+            name="title"
+            id="title"
+            required
+            placeholder="Insira um título"
+            onChange={(e) => setTitle(e.target.value)}
+            value={title}
+          />
+        </label>
+        <label>
+          <span>URL da imagem:</span>
+          <input
+            type="text"
+            name="image"
+            id="image"
+            required
+            placeholder="Insira uma imagem para o seu post"
+            onChange={(e) => setImage(e.target.value)}
+            value={image}
+          />
+        </label>
+        <label>
+          <span>Conteúdo:</span>
+          <textarea
+            onChange={(e) => setBody(e.target.value)}
+            name="body"
+            placeholder="Insira o conteúdo do post"
+            value={body}
+            required
+          ></textarea>
+        </label>
+        <label>
+          <span>URL da imagem:</span>
+          <input
+            type="text"
+            name="tags"
+            id="tags"
+            required
+            placeholder="Insira as tags separadas por vírgula"
+            onChange={(e) => setTags(e.target.value)}
+            value={tags}
+          />
+          <button type="submit" className="btn">
+            Cadastrar
+          </button>
+        </label>
+        {/* {!loading && (
+          
+        )}
+        {loading && (
+          <button type="submit" className="btn">
+            Aguarde...
+          </button>
+        )}
+        {formError && <p className="error">{formError}</p>} */}
+      </form>
+    </div>
+  );
+};
+
+export default CreatePost;
